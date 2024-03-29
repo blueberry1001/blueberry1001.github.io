78b466b633df68ab1ef254386d82662e3429f755
blueberry <kubarun317@gmail.com>
2024-03-29 22:53:33 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,65 @@
+import "./App.css";
+
+import { BrandGithub, BrandTwitter, BrandYoutube } from "tabler-icons-react";
+
+function App() {
+  const games: { name: string; url: string }[] = [
+    { name: "スイカゲーム", url: "blueberry1001.github.io/suica_game" },
+    { name: "ヘビゲーム", url: "blueberry1001.github.io/snake" },
+  ];
+  return (
+    <>
+      <h1>Blueberry</h1>
+      <div className="Links">
+        <a
+          href="https://twitter.com/bluebery1001"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BrandTwitter size="3rem" />
+        </a>
+        <a
+          href="https://github.com/blueberry1001"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BrandGithub size="3rem" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCOJw7xtcqd3EbO9h-jyWrVg1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BrandYoutube size="3rem" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCOJw7xtcqd3EbO9h-jyWrVg1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          note
+        </a>
+      </div>
+      <div>
+        <h2>自己紹介</h2>
+        <div>
+          <p>競技プログラミングをしています！</p>
+        </div>
+      </div>
+      <div>
+        <h2>作ったゲーム</h2>
+        <div>
+          <ul>
+            {games.map((game) => (
+              <li key={game.name}>
+                <a href={game.url}>{game.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
