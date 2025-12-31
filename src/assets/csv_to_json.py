874964dc488fb140398db115fec1af780294a8c0
blueberry1001 <kubarun317@gmail.com>
2025-12-31 23:47:17 +0900

    update json

--- src/assets/csv_to_json.py
+++ src/assets/csv_to_json.py
@@ -7,6 +7,9 @@ OUTPUT = "temp/thinkers.json"
 LIST_FIELDS = {"books","description"}
 INT_FIELDS = {"exam_priority"}
 
+key_list = ["id","name","books","religion","era","region"]
+listkey_key = ["books","description"]
+
 def csv_to_json():
     result = []
 
@@ -16,27 +19,30 @@ def csv_to_json():
         for row in reader:
             item = {}
 
+            for key in key_list:
+                if key not in row:
+                    item[key] = ""
+            
+            for key in listkey_key:
+                if key not in row:
+                    item[key] = []
+
             for key, val in row.items():
                 val = val.strip()
-
-                if val == "":
-                    continue
-
                 if key in LIST_FIELDS:
-                    item[key] = [x.strip() for x in val.split(";")]
+                    if val == "":
+                        item[key] = []
+                    else:
+                        item[key] = [x.strip() for x in val.split(";")]
                 elif key in INT_FIELDS:
                     item[key] = int(val)
                 else:
                     item[key] = val
 
-            # 最低限の検証
-            if "name" not in item:
-                raise ValueError("name is required")
-
             result.append(item)
-
     with open(OUTPUT, "w", encoding="utf-8") as f:
         json.dump(result, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     csv_to_json()
+    print("Done")
